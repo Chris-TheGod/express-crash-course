5f963f090c928aa27c183a07d42b0ed5af4ba4e2
Kristjan Lääne <xonproject@gmail.com>
2025-03-18 19:23:16 +0200

    sendFile()

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
+// setup static folder
+
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
 });
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
