5558854307abdb325df4748714ffde3bcf0e78ca
Kristjan Lääne <xonproject@gmail.com>
2025-03-18 19:24:47 +0200

    static web server

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ const path = require('path');
 const app = express();
 
 // setup static folder
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'about.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(8000, () => console.log(`Server is running on port 8000`));
